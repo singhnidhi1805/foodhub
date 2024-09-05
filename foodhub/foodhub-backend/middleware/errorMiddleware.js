640a929c99a7ab073679fcf53418d1899f6e7772
Nidhi kumari <115169748+singhnidhi1805@users.noreply.github.com>
2024-09-05 11:29:13 +0530

    Delete foodhub directory

--- foodhub/foodhub-backend/middleware/errorMiddleware.js
+++
@@ -1,7 +0,0 @@
-exports.errorHandler = (err, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    res.status(statusCode).json({
-        message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack
-    });
-};
